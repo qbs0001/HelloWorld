16d0965d5c9ee3e77b4b2dfcbe2a0745aaa520e8
qbs0001 <qbs0001@sample.com>
2020-04-16 13:37:56 +0900

    HelloWorldのラベルを追加した。

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 0001 QBS on 2020/04/16.
+//  Copyright © 2020 qbs0001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
